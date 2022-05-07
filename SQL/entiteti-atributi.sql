7fc04bc66e521064ad8a438ce9a2768e574f7128
Filip Kovac <filip.kvch@gmail.com>
2022-05-07 12:33:18 +0200

    Update entiteti-atributi.sql
    Dodano: Taxi služba

--- SQL/entiteti-atributi.sql
+++ SQL/entiteti-atributi.sql
@@ -159,3 +159,35 @@ create table posao (
     trajanje_posla varchar(50),
     lokacija_posla varchar(50)
 );
+
+# Taxi služba
+
+drop database if exists taxi_sluzba;
+create database taxi_sluzba;
+use taxi_sluzba;
+
+create table vozilo (
+    proizvodjac varchar(50),
+    vrsta_vozila varchar(50),
+    prijedjeni_kilometri varchar(50),
+    maksimalni_broj_putnika varchar(50)
+);
+
+create table vozac (
+    osoba varchar(50),
+    IBAN varchar(50),
+    vozilo varchar(50)
+);
+
+create table putnik (
+    osoba varchar(50),
+    polaziste varchar(50),
+    odrediste varchar(50)
+);
+
+create table osoba (
+    ime varchar(50),
+    prezime varchar(50),
+    OIB varchar(50),
+    email varchar(50)
+);
